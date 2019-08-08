ec360803683092ca2c8994d60055dc8d3f4eb906
Max Lee <maxolee23@gmail.com>
2019-08-08 16:27:24 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,7 @@ class Person
   #your code here
   attr_accessor :name, :birthday, :hair_color, :eye_color, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   def initialize(parameters)
-    parameters.each do |k, v|
-      self.send("#{k}=",v)
+    parameters.each do |key, value|
+      self.send("#{key}=",value)
   end
 end
